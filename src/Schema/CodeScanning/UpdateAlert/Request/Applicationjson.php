e88fb1a0e440f012c1ce01706df1e1448f2be22b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-03-21 17:20:28 +0100

    Use enum for ecample data when available

--- src/Schema/CodeScanning/UpdateAlert/Request/Applicationjson.php
+++ src/Schema/CodeScanning/UpdateAlert/Request/Applicationjson.php
@@ -13,7 +13,7 @@ final readonly class Applicationjson
     public const SCHEMA_JSON = '{"required":["state"],"type":"object","properties":{"state":{"enum":["open","dismissed"],"type":"string","description":"Sets the state of the code scanning alert. You must provide `dismissed_reason` when you set the state to `dismissed`."},"dismissed_reason":{"enum":[null,"false positive","won\'t fix","used in tests"],"type":["string","null"],"description":"**Required when the state is dismissed.** The reason for dismissing or closing the alert."}}}';
     public const SCHEMA_TITLE = '';
     public const SCHEMA_DESCRIPTION = '';
-    public const SCHEMA_EXAMPLE_DATA = '{"state":"generated_state_null","dismissed_reason":"generated_dismissed_reason_null"}';
+    public const SCHEMA_EXAMPLE_DATA = '{"state":"open","dismissed_reason":"generated_dismissed_reason_null"}';
     /**
      * state: Sets the state of the code scanning alert. You must provide `dismissed_reason` when you set the state to `dismissed`.
      * dismissed_reason: **Required when the state is dismissed.** The reason for dismissing or closing the alert.
