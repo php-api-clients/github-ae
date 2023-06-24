be521fc9e2dd04f2a532d1e440562f27ec315a8a
WyriHaximus <WyriHaximus@users.noreply.github.com>
2023-06-24 14:39:37 +0000

    [GitHubAE] Update to 1.1.4 (hash: cbd87d3c7c7ad8ae59d7c5749c53f099) from 1.1.4 (hash: a4e17c0e4309494e07cadc673434db27) Detected Schema changes:
    ├──Paths
    └─┬Components
      ├──[-] schemas (51498:7)❌
      └─┬code-scanning-alert-state
        └──[+] enum (51502:9)
    
    Date: 06/24/23 | Commit: New: etc/specs/GitHubAE/previous.spec.yaml, Original: etc/specs/GitHubAE/current.spec.yaml
    Document Element | Total Changes | Breaking Changes
    paths            | 0             | 0
    components       | 2             | 1
    ❌ 1 Breaking changes out of 2
    
    ERROR: breaking changes discovered

--- src/Schema/CodeScanningAlertStateQuery.php
+++ src/Schema/CodeScanningAlertStateQuery.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace ApiClients\Client\GitHubAE\Schema;
 
-final readonly class CodeScanningAlertState
+final readonly class CodeScanningAlertStateQuery
 {
-    public const SCHEMA_JSON         = '{"enum":["open","dismissed","fixed"],"type":"string","description":"State of a code scanning alert."}';
+    public const SCHEMA_JSON         = '{"enum":["open","closed","dismissed","fixed"],"type":"string","description":"State of a code scanning alert."}';
     public const SCHEMA_TITLE        = '';
     public const SCHEMA_DESCRIPTION  = 'State of a code scanning alert.';
     public const SCHEMA_EXAMPLE_DATA = '[]';
